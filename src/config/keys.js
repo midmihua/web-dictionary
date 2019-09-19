927560cedbad6e4001ec9e2c42f5b94e618df5c7
dmytro.mykhailenko <mdmprog@gmail.com>
2019-09-19 17:39:09 +0300

    keys corrected

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,6 @@
-module.exports.mongoURI = '';
+module.exports.mongoURI =
+    'mongodb+srv://webdic:webdic123@cluster0-n7b47.mongodb.net/webdictionary?retryWrites=true&w=majority';
 
 module.exports.httpPort = 5000;
 
-module.exports.jwtSecret = '';
+module.exports.jwtSecret = 'somesupersecret';
